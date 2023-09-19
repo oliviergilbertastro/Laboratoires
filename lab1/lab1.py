f66fb4283e5c0079137671e975b4dbf7d70f3e71
OlivierGilbert <olgil12@ulaval.ca>
2023-09-19 11:48:59 -0400

    data_analysis

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,15 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pylab import genfromtxt
+import pandas as pd
+
+def conv(x):
+    return x.replace(',', '.').encode()
 
 def get_values_from_file(filename):
-    values = genfromtxt(open("swift_xrt_lc.txt", "r"))
+    values = pd.read_csv(filename, delimiter=";", decimal=",", skiprows=22)
     return values
 
 def create_hist(values, combinaison="Inox-Alu", bins=200):
     counts, bins = np.histogram(values, bins=bins)
     plt.stairs(counts, bins)
-    plt.show()
     plt.suptitle(f'Histogramme pour la combinaison {combinaison}', size=17)
     plt.xlabel(r'Amplitude [V]', size=17)
+    plt.show()
 
+
+#Start data analysis
+inox_alu = get_values_from_file("lab1/lab1_InoxAlu_1010_09_19_2023.lvm")
+print(inox_alu)
+create_hist(inox_alu)
+
+inox_alu = get_values_from_file("lab1/lab1_InoxAlu_19_09_2023.lvm")
+print(inox_alu)
+create_hist(inox_alu)
