dee45518e7186c4a914fcc56221d33d2878faf02
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2023-11-09 18:07:17 -0500

    incertitude

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -17,8 +17,8 @@ def create__osci_plot(filename, name):
     y = np.array(values.iloc[:, 1])
     z = np.array(values.iloc[:, 2])
 
-    
-    
+
+
     ax1 = plt.subplot(111)
     ticklabels = ax1.get_xticklabels()
     ticklabels.extend( ax1.get_yticklabels() )
@@ -46,52 +46,60 @@ def create__osci_plot(filename, name):
 #On créé une liste des résistances mesurées et une liste des tensions mesurées (SANS CONDENSATEUR):
 
 resistance_sansC = []
+resistance_sansC_stdev = []
 tension_sansC = []
+tension_sansC_stdev = []
 
 for i in range(14):
     res = np.array(get_values_from_file(f"lab5/SANS_C/resistance_{i}.lvm").iloc[:, 1])
     ten = np.array(get_values_from_file(f"lab5/SANS_C/tension_efficace_{i}.lvm").iloc[:, 1])
 
-    resistance_sansC.extend(res)
-    tension_sansC.extend(ten)
+    #On calcule la val. médianne et l'incertitude (stdev):
+
+
+
+    resistance_sansC.append(np.median(res))
+    tension_sansC.append(np.median(ten))
+    resistance_sansC_stdev.append(np.std(res))
+    tension_sansC_stdev.append(np.std(ten))
+
+
 
 puissance_moy_sansC = []
+puissance_moy_sansC_stdev = []
 for i in range(len(resistance_sansC)):
     puissance_moy_sansC.append(tension_sansC[i]**2/resistance_sansC[i])
+    #Propagate the uncertainty to the power:
+    puissance_moy_sansC_stdev.append(puissance_moy_sansC[i]*np.sqrt((resistance_sansC_stdev[i]/resistance_sansC[i])**2+(tension_sansC_stdev[i]/tension_sansC[i])**2))
+
+
 
 ax1 = plt.subplot(111)
 ticklabels = ax1.get_xticklabels()
 ticklabels.extend( ax1.get_yticklabels() )
 for label in ticklabels:
     label.set_fontsize(14)
-plt.plot(resistance_sansC, puissance_moy_sansC, ".")
+plt.errorbar(resistance_sansC, puissance_moy_sansC, puissance_moy_sansC_stdev, resistance_sansC_stdev, ".")
 plt.xscale('log')
 plt.ylabel(r'P$_\mathrm{moy}$ [W]', size=17)
 plt.xlabel(r'Résistance [$\Omega$]', size=17)
 plt.show()
 
-
-
-
-x_sim = np.linspace(10,250)
-
-#Model of V(R):
-
-def tension(R,I):
-    return R*I
-
-res=  curve_fit(tension, resistance_sansC, tension_sansC)
 ax1 = plt.subplot(111)
 ticklabels = ax1.get_xticklabels()
 ticklabels.extend( ax1.get_yticklabels() )
 for label in ticklabels:
     label.set_fontsize(14)
-plt.plot(resistance_sansC, tension_sansC, ".", label="data")
-plt.plot(x_sim, tension(x_sim, res[0]), "--", label="fit")
-#plt.xscale('log')
+plt.plot(resistance_sansC, tension_sansC, ".")
+plt.xscale('log')
 plt.ylabel(r'Tension [V]', size=17)
 plt.xlabel(r'Résistance [$\Omega$]', size=17)
 plt.show()
+
+
+
+
+x_sim = np.linspace(10,250, 100)
 
 
 #Model of P(R,V):
@@ -99,25 +107,75 @@ plt.show()
 def puissance(known_params, R_S):
     return known_params[0]*np.abs(known_params[1])**2*(1/(known_params[0]+R_S)**2)
 
+def puissance(R_ch, R_S):
+    return R_ch*np.abs(1/np.sqrt(2))**2*(1/(R_ch+R_S)**2)
+
 #Fitting power:
 
 
-res = curve_fit(puissance, [resistance_sansC, tension_sansC], puissance_moy_sansC)
-print(res[0])
+#MCMC:
+from tqdm import tqdm
+from random import gauss
+
+def resample_array(current, current_std):
+    new = []
+    for s in range(0, len(current)):
+        if current_std[s] > 0:
+            new.append(gauss(current[s], current_std[s]))
+        else:
+            new.append(current[s])
+    return np.asarray(new)
+
+resistance_fit = []
+tension_efficace_fit = []
+for i in tqdm(range(10000)):
+    res = curve_fit(puissance, resample_array(resistance_sansC, resistance_sansC_stdev), resample_array(puissance_moy_sansC, puissance_moy_sansC_stdev))[0]
+    #tension_efficace_fit.append(res[0])
+    resistance_fit.append(res[0])
+
+#Make histograms of fitted params
+ax1 = plt.subplot(111)
+counts, bins = np.histogram(resistance_fit, bins=200)
+ax1.stairs(counts, bins)
+ax1.set_ylabel(r'Counts', size=17)
+ax1.set_xlabel(r'Résistance [$\Omega$]', size=17)
+plt.axvline(x = np.quantile(resistance_fit, 0.1585), color = 'blue', linestyle = '-', label=r'1$\sigma$')
+plt.axvline(x = np.quantile(resistance_fit, 0.8415), color = 'blue', linestyle = '-')
+plt.axvline(x = np.quantile(resistance_fit, 0.0225), color = 'green', linestyle = '-', label=r'2$\sigma$')
+plt.axvline(x = np.quantile(resistance_fit, 0.9775), color = 'green', linestyle = '-')
+plt.axvline(x = np.quantile(resistance_fit, 0.0015), color = 'orange', linestyle = '-', label=r'3$\sigma$')
+plt.axvline(x = np.quantile(resistance_fit, 0.9985), color = 'orange', linestyle = '-')
+plt.legend()
+plt.show()
+
+res = curve_fit(puissance, resistance_sansC, puissance_moy_sansC, sigma=puissance_moy_sansC_stdev)
+print(res)
+
+
+found_sim = []
+for i in range(len(x_sim)):
+    found_sim.append(puissance(x_sim[i], res[0][0]))
+
+#print(np.array(puissance_moy_sansC)/np.array(pow_2))
 
 ax1 = plt.subplot(111)
 ticklabels = ax1.get_xticklabels()
 ticklabels.extend( ax1.get_yticklabels() )
 for label in ticklabels:
     label.set_fontsize(14)
-plt.plot(resistance_sansC, puissance_moy_sansC, ".", label="data")
-plt.plot(x_sim, puissance(x_sim, res[0]), "--", label="fit")
+plt.errorbar(resistance_sansC, puissance_moy_sansC, puissance_moy_sansC_stdev, resistance_sansC_stdev, ".", label="data")
+plt.plot(x_sim, found_sim, label="fit")
+plt.legend()
 plt.xscale('log')
 plt.ylabel(r'P$_\mathrm{moy}$ [W]', size=17)
 plt.xlabel(r'Résistance [$\Omega$]', size=17)
 plt.show()
 
+sigma_1 = ((np.quantile(resistance_fit, 0.8415)-np.quantile(resistance_fit, 0.50))+(np.quantile(resistance_fit, 0.5)-np.quantile(resistance_fit, 0.1585)))/2
+sigma_2 = ((np.quantile(resistance_fit, 0.9775)-np.quantile(resistance_fit, 0.50))+(np.quantile(resistance_fit, 0.5)-np.quantile(resistance_fit, 0.0225)))/2
+sigma_3 = ((np.quantile(resistance_fit, 0.9985)-np.quantile(resistance_fit, 0.50))+(np.quantile(resistance_fit, 0.5)-np.quantile(resistance_fit, 0.0015)))/2
 
-
-
-print(res)
+print(f"R_S:     {np.median(resistance_fit)}")
+print(f"        1 sigma: {sigma_1}")
+print(f"        2 sigma: {sigma_2}")
+print(f"        3 sigma: {sigma_3}")
