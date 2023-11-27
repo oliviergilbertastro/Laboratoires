50b2420340faa8a31d8b8e2f448db961f39351ea
OlivierGilbert <olgil12@ulaval.ca>
2023-11-27 14:05:57 -0500

    tout fait

--- dev2/dev2_2.py
+++ dev2/dev2_2.py
@@ -7,23 +7,19 @@ def get_values_from_file(filename):
     return values
 
 
-
+print(np.angle(1j))
 
 #freq = np.linspace(1, 15E6, 1000)
 
 freq = np.logspace(np.log10(1), np.log10(15E6), 1000, 10)
 
-hh = []
 gain = []
 ph = []
+hh = []
 
 def h(f):
     w = 2*np.pi*f
-    return (1E6-0.001*w**2)*(1.27E6-0.00127*w**2-270j*w)/(72900*w**2+(1.27E6-0.00127*w**2)**2)
-
-def g(f):
-    w = 2*np.pi*f
-    return np.sqrt((1E6-0.001*w**2)**2*(1.27E6-0.00127*w**2)**2+72900*w**2*(1E6-0.001*w**2)**2)/(72900*w**2+(1.27E6-0.00127*w**2)**2)
+    return 12*((1-12*((((1/(-2j/(w*1E-6)+12))+1/270+1j*w*1E-6))/(1+12*((1/(-2j/(w*1E-6)+12))+1/270+1j*w*1E-6))))/(-2j/(w*1E-6)+12))
 
 def g(f):
     return np.abs(h(f))
@@ -36,7 +32,6 @@ for i in freq:
     ph.append(phase(i))
     hh.append(h(i))
 
-
 ax1 = plt.subplot(111)
 ticklabels = ax1.get_xticklabels()
 ticklabels.extend( ax1.get_yticklabels() )
@@ -46,7 +41,7 @@ plt.plot(freq, gain, '.', color='purple')
 plt.xscale('log')
 plt.ylabel(r'Gain [dB]', size=17)
 plt.xlabel(r'Fréquence [Hz]', size=17)
-plt.savefig(r'dev2/bode_gain_1.pdf', format="pdf", bbox_inches="tight")
+plt.savefig(r'dev2/bode_gain_2.pdf', format="pdf", bbox_inches="tight")
 plt.show()
 
 ax1 = plt.subplot(111)
@@ -58,5 +53,5 @@ plt.plot(freq, ph, '.', color='purple')
 plt.xscale('log')
 plt.ylabel(r'Phase $\varphi$ [rad]', size=17)
 plt.xlabel(r'Fréquence [Hz]', size=17)
-plt.savefig(r'dev2/bode_phase_1.pdf', format="pdf", bbox_inches="tight")
+plt.savefig(r'dev2/bode_phase_2.pdf', format="pdf", bbox_inches="tight")
 plt.show()
