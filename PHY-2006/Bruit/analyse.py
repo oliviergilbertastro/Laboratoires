69cd2e6307a417b323fe01b43e26a31b3dd4381f
OlivierGilbert <olgil12@ulaval.ca>
2024-01-30 12:10:58 -0500

    bruitBruit

--- PHY-2006/Bruit/analyse.py
+++ PHY-2006/Bruit/analyse.py
@@ -5,18 +5,6 @@ from scipy.optimize import curve_fit
 
 data = np.loadtxt('PHY-2006/Bruit/signalSimu.txt')
 
-plt.plot(data)
-plt.show()
-
-#On se créé un array de chaque point:
-modif = []
-for i in range(len(data)):
-    if i > 100:
-        modif[i % 100].append(data[i])
-    else:
-        modif.append([])
-        modif[i % 100].append(data[i])
-
 cycles = [[],]
 last_dim = 0
 
@@ -27,6 +15,16 @@ for i in range(len(data)):
     cycles[dim].append(data[i])
     last_dim = dim
 
-plt.plot(np.mean(cycles, axis=0))
+
+
+ax1 = plt.subplot(131)
+ax2 = plt.subplot(132, sharex=ax1, sharey=ax1)
+ax3 = plt.subplot(133, sharex=ax1, sharey=ax1)
+
+ax1.plot(data[:100])
+ax1.set_title('Données brutes')
+ax2.plot(np.mean(cycles, axis=0))
+ax2.set_title(f'Données moyennées sur {last_dim+1} cycles')
+ax3.plot(np.median(cycles, axis=0))
+ax3.set_title(f'Données "médiannées" sur {last_dim+1} cycles')
 plt.show()
-print(cycles)
--- PHY-2006/Bruit/simulation.py
+++ PHY-2006/Bruit/simulation.py
@@ -28,7 +28,7 @@ signal = SignalBruit(
                         tension=5,
                         period=100,
                         noise_mean=0,
-                        noise_std=0.05,
-                        number_of_cycles=50
+                        noise_std=0.15,
+                        number_of_cycles=10
                         )
 signal.saveTo('PHY-2006/Bruit/signalSimu.txt')
