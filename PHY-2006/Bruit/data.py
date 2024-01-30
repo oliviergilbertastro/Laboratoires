625c6c028c69e042bc2e40c360f362b49377ad6b
OlivierGilbert <olgil12@ulaval.ca>
2024-01-30 16:05:44 -0500

    byebye

--- PHY-2006/Bruit/data.py
+++ PHY-2006/Bruit/data.py
@@ -16,7 +16,7 @@ for k in range(3,7):
     for n in range(6):
         print(len(data))
         for i, val in enumerate(file.iloc[:, n]):
-            if np.isnan(val) or i < 2500 or i > 22500:
+            if np.isnan(val) or i < 2500 or i > 22500 or val > 0.76:
                 pass
             else:
                 data.append(val)
@@ -27,12 +27,54 @@ for i in range(len(data)):
 
 for i in clusters_of_data:
     if np.mean(i) > 0.71:
-        data_highs.extend(i)
+        data_highs.append(i)
     else:
-        data_lows.extend(i)
+        data_lows.append(i)
 
 #for i in range(len(clusters_of_data)-1):
 #    plt.plot(range(i*5000, (i+1)*5000), clusters_of_data[i])
-plt.plot(range(len(data_highs)), data_highs)
-plt.plot(range(len(data_highs), len(data_highs)+len(data_lows)), data_lows)
+#plt.plot(range(len(data_highs)), data_highs)
+#plt.plot(range(len(data_highs), len(data_highs)+len(data_lows)), data_lows)
+
+
+#print(np.mean(data_highs), np.mean(data_lows))
+
+
+analyse = []
+
+for i in range(int(len(clusters_of_data)/2)):
+    analyse.extend(data_lows[i])
+    analyse.extend(data_highs[i])
+
+
+
+
+
+
+cycles = [[],]
+last_dim = 0
+
+data = analyse[160000:]
+plt.plot(data)
+plt.show()
+
+for i in range(len(data)):
+    dim = int(i // 10000)
+    if dim != last_dim:
+        cycles.append([])
+    cycles[dim].append(data[i])
+    last_dim = dim
+
+cycles = cycles[:-1]
+
+ax1 = plt.subplot(131)
+ax2 = plt.subplot(132, sharex=ax1, sharey=ax1)
+ax3 = plt.subplot(133, sharex=ax1, sharey=ax1)
+
+ax1.plot(data[:10000])
+ax1.set_title('Données brutes')
+ax2.plot(np.mean(cycles, axis=0))
+ax2.set_title(f'Données moyennées sur {last_dim+1} cycles')
+ax3.plot(np.median(cycles, axis=0))
+ax3.set_title(f'Données "médiannées" sur {last_dim+1} cycles')
 plt.show()
