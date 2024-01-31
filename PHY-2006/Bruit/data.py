87c2e2bdec62fa15f3177dd3605444aee2606c27
OlivierGilbert <olgil12@ulaval.ca>
2024-01-31 13:19:03 -0500

    std

--- PHY-2006/Bruit/data.py
+++ PHY-2006/Bruit/data.py
@@ -14,7 +14,6 @@ data_lows = []
 for k in range(3,7):
     file = get_values_from_file(f'PHY-2006/Bruit/labBruit_{k}.lvm')
     for n in range(6):
-        print(len(data))
         for i, val in enumerate(file.iloc[:, n]):
             if np.isnan(val) or i < 2500 or i > 22500 or val > 0.76:
                 pass
@@ -38,7 +37,29 @@ for i in clusters_of_data:
 
 
 #print(np.mean(data_highs), np.mean(data_lows))
+allHIGH = []
+allLOW = []
+for i in data_highs:
+    allHIGH.extend(i)
+for i in data_lows:
+    allLOW.extend(i)
+print('BEFORE:')
+print('STD low:', np.std(allLOW))
+print('STD high:', np.std(allHIGH))
+print('--------------------')
 
+ax1, ax2 = plt.subplot(121), plt.subplot(122)
+counts, bins = np.histogram(allLOW, bins=200)
+ax1.stairs(counts, bins)
+counts, bins = np.histogram(allHIGH, bins=200)
+ax2.stairs(counts, bins)
+ax1.set_title('Low')
+ax2.set_title('High')
+ax1.set_xlabel(r'Tension (V)', size=17)
+ax1.set_ylabel(r'Count', size=17)
+ax2.set_xlabel(r'Tension (V)', size=17)
+ax2.set_ylabel(r'Count', size=17)
+plt.show()
 
 analyse = []
 
@@ -80,9 +101,16 @@ ax3.set_title(f'Données "médiannées" sur {last_dim+1} cycles')
 plt.show()
 
 
-print('LOW:', np.mean(np.mean(cycles, axis=0)[500:4500]))
-print('HIGH:', np.mean(np.mean(cycles, axis=0)[5500:9500]))
-print('Difference:', np.mean(np.mean(cycles, axis=0)[5500:9500])-np.mean(np.mean(cycles, axis=0)[500:4500]))
+low_mean, high_mean = np.mean(np.mean(cycles, axis=0)[200:4500]), np.mean(np.mean(cycles, axis=0)[5200:9500])
+low_std, high_std = np.std(np.mean(cycles, axis=0)[200:4500]), np.std(np.mean(cycles, axis=0)[5200:9500])
+print('LOW:', low_mean)
+print('HIGH:', high_mean)
+print('Difference:', high_mean-low_mean)
+print('STD low:', low_std)
+print('STD high:', high_std)
 plt.plot(np.mean(cycles, axis=0)[200:4500])
 plt.plot(np.mean(cycles, axis=0)[5200:9500])
+plt.hlines([low_mean, high_mean], 0, 4300, linestyles='dashed', colors=['red', 'green'])
+plt.fill_between(range(0,4300), low_mean-low_std, low_mean+low_std, color='red', alpha=0.4)
+plt.fill_between(range(0,4300), high_mean-high_std, high_mean+high_std, color='green', alpha=0.4)
 plt.show()
