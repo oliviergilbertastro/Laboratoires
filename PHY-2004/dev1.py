731e9eac7c0e931f3dec8c8d059cee738158729c
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2024-02-02 17:55:19 -0500

    dev1optique

---
+++ PHY-2004/dev1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+k_0 = 1
+gamma = 1
+w_0 = 10
+n = 1
+
+
+def alpha(w):
+    return 2*k_0*(1/(2*n)*(0.1*gamma*w)/((w_0**2)**2+(gamma*w)**2))
+
+
+
+x = np.linspace(0, 100, 1000)
+y = alpha(x)
+
+plt.plot(x, y)
+plt.xlabel(r'$\omega$', fontsize=17)
+plt.ylabel(r'$\alpha$', fontsize=17)
+plt.show()
