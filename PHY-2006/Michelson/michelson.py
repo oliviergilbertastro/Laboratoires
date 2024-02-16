cce74a0c01d2056bbef888494bd9d3812e11f2dc
OlivierGilbert <olgil12@ulaval.ca>
2024-02-16 10:05:26 -0500

    da

--- PHY-2006/Michelson/michelson.py
+++ PHY-2006/Michelson/michelson.py
@@ -8,7 +8,7 @@ from scipy.fft import fft, fftfreq
 data_HeNe = pd.read_csv('PHY-2006/Michelson/Data/jnterferogrammeHeNe.txt', delimiter="\t", decimal=".", skiprows=17, encoding='latin-1', engine='python')
 
 pos_HeNe, sig_HeNe = data_HeNe.iloc[:, 1], data_HeNe.iloc[:, 2]
-marche_HeNe = np.array(pos_HeNe)*0.2
+marche_HeNe = np.array(pos_HeNe)*2
 
 
 
@@ -24,7 +24,31 @@ indice_max_amplitude = np.argmax(np.abs(fourier.imag))
 
 freq_res = freq[indice_max_amplitude]
 
+
+print(indice_max_amplitude)
 print(f'Fréq résonance: {np.abs(freq_res)} [1/m]')
-print(f'Longueur donde: {(1/np.abs(freq))} [m]')
+print(f'Longueur donde: {(1/np.abs(freq[indice_max_amplitude]))} [m]')
 plt.plot((1/freq)*1E9, np.abs(fourier))
+plt.xlabel(r'$\lambda$ [nm]')
+plt.show()
+
+
+#On fit un sinus:
+from scipy.optimize import curve_fit
+
+def sinus(x, freq, phi, c):
+    return c*np.sin(2*np.pi*freq*(x)-phi)-645.2327517
+
+res = curve_fit(sinus, marche_HeNe, sig_HeNe)[0]
+print(res)
+print('Longueur onde:', 299792458/np.abs(res[0]))
+
+x_sim = np.linspace(59, 70, 1000)
+y_sim = sinus(x_sim, res[0], res[1], res[2])
+
+plt.plot(marche_HeNe, sig_HeNe, label='data')
+plt.plot(x_sim, y_sim, label='fit')
+plt.xlabel(r'$\delta$ [$\mathrm{\mu m}$]')
+plt.ylabel(r'Tension [mV]')
+plt.legend()
 plt.show()
