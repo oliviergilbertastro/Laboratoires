638de549236b8dc52cad2667f57e62b5dedde505
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2024-03-07 18:55:46 -0500

    phys num

---
+++ PHY-3500/tp2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+#A = QR
+I_i = 0
+H_mi = 0
+
+def sgn(x):
+    return -1 if x < 0 else 1
+
+def H(m,i):
+    alpha = -sgn()
+    return np.sign()
+
+def Q(i):
+    return np.array([[np.identity(i), 0], [0, H(m,i)]])
+
+def householder_qr(A):
+    Q, R = 0, 0
+    return Q, R
+
+
