5513a9afde39f70123460dcdd5cddd1b331c4e22
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2024-11-16 18:53:41 -0500

    dev5 relgen

---
+++ PHY-4201/dev5.py
@@ -0,0 +1,21 @@
+"""
+#4d du devoir 5 de relgen:
+
+Pour un photon, montrer numériquement à partir de l'intégrale obtenue en
+(4c) que le photon orbite entre un et deux tours autour du trou noir avant de s'échapper
+vers l'infini pour r_min ∈ (1.502, 1.545)R_S.
+
+"""
+
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+
+R_S = 1 # Rayon Schwarzchild
+r_mins = np.linspace(1.502, 1.545, 10)*R_S
+
+for r_min in r_mins:
+    pass
+
