75840a43abeb49725e1e0c1f4e6bbb834d7643e1
OlivierGilbert <olgil12@ulaval.ca>
2025-01-27 14:07:39 -0500

    Analyse des gaussiennes

--- PHY-3004/SPEC_X/resolution_channels.py
+++ PHY-3004/SPEC_X/resolution_channels.py
@@ -34,37 +34,44 @@ plt.show()
 
 
 
-gaussiennes = [data[0][770:902], data[1][770:902]]
-plt.plot(range(len(gaussiennes[0])), gaussiennes[0], label="256")
-plt.plot(range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]), label="4096")
-plt.legend()
-plt.xlabel("Channel")
-plt.ylabel("Count")
-plt.show()
 
-def gaussian(x, sig, b, mu):
+
+def gaussian(x, sig, b, mu, c):
     return (
-        b / (np.sqrt(2.0 * np.pi) * sig) * np.exp(-np.power((x-mu) / sig, 2.0) / 2)
+        b / (np.sqrt(2.0 * np.pi) * sig) * np.exp(-np.power((x-mu) / sig, 2.0) / 2) + c
     )
-
 
 from scipy.optimize import curve_fit
 
-res0 = curve_fit(gaussian, range(len(gaussiennes[0])), gaussiennes[0])[0]
-res1 = curve_fit(gaussian, range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]))[0]
+for pics in [(770,902),(1045,1124),(1519,1638),(3271,3700)]:
 
-print(res0)
-print(res1)
+    gaussiennes = [data[0][pics[0]:pics[1]], data[1][pics[0]:pics[1]]]
+    plt.plot(range(len(gaussiennes[0])), gaussiennes[0], label="256")
+    plt.plot(range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]), label="4096")
+    plt.legend()
+    plt.xlabel("Channel")
+    plt.ylabel("Count")
+    plt.show()
 
-for res in [res0, res1]:
-    print(f"FWHM = {2*np.sqrt(2*np.log(2))*res[0]}")
 
-x_sim = np.linspace(0, len(gaussiennes[0]), 1000)
-plt.plot(range(len(gaussiennes[0])), gaussiennes[0], label="256")
-plt.plot(range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]), label="4096")
-plt.plot(x_sim, gaussian(x_sim, res0[0], res0[1], res0[2]), label="256")
-plt.plot(x_sim, gaussian(x_sim, res1[0], res1[1], res1[2]), label="4096")
-plt.legend()
-plt.xlabel("Channel")
-plt.ylabel("Count")
-plt.show()
+
+    res0 = curve_fit(gaussian, range(len(gaussiennes[0])), gaussiennes[0], p0=[1.46726127e+01, 2.59850085e+06, len(gaussiennes[0])/2, 5.4e+3], bounds=[(0,0,0,0),(np.inf,np.inf,np.inf,np.inf)])[0]
+    res1 = curve_fit(gaussian, range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]), p0=[1.22854407e+01, 2.44366868e+06, len(gaussiennes[1])/2, 5.4e+3], bounds=[(0,0,0,0),(np.inf,np.inf,np.inf,np.inf)])[0]
+
+    print(res0)
+    print(res1)
+
+    for res in [res0, res1]:
+        print(f"FWHM = {2*np.sqrt(2*np.log(2))*res[0]}")
+
+    x_sim = np.linspace(0, len(gaussiennes[0]), 1000)
+    #plt.plot(range(len(gaussiennes[0])), gaussiennes[0], ".", label="256", color="blue")
+    #plt.plot(range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]), ".", label="4096", color="orange")
+    plt.bar(range(len(gaussiennes[0])), gaussiennes[0], width=1, color="blue", alpha=0.4, label="256")
+    plt.bar(range(len(gaussiennes[1])), gaussiennes[1]*np.max(data[0])/np.max(data[1]), width=1, color="red", alpha=0.4, label="4096")
+    plt.plot(x_sim, gaussian(x_sim, res0[0], res0[1], res0[2], res0[3]), color="blue")
+    plt.plot(x_sim, gaussian(x_sim, res1[0], res1[1], res1[2], res1[3]), color="red")
+    plt.legend()
+    plt.xlabel("Channel")
+    plt.ylabel("Count")
+    plt.show()
