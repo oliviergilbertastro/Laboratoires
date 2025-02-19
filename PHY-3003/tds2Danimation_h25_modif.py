8e6a447876ae783ee4d192d7cce756c7ab2cf668
Olivier Gilbert <olivier.gilbert.6@ulaval.ca>
2025-02-19 00:53:57 -0500

    Drude

--- PHY-3003/tds2Danimation_h25_modif.py
+++ PHY-3003/tds2Danimation_h25_modif.py
@@ -19,8 +19,9 @@ import pickle
 # win = 500 # peut aider à définir la taille d'un autre objet visuel comme un histogramme proportionnellement à la taille du canevas.
 
 # Déclaration de variables influençant le temps d'exécution de la simulation
-Nelectrons = 200  # change this to have more or fewer atoms
-dt = 1E-8  # pas d'incrémentation temporel (plus petit pour les électrons)
+Nelectrons = 200  # change this to have more or fewer electrons
+Ncores = 9  # change this to have more or fewer cores
+dt = 2E-8  # pas d'incrémentation temporel (plus petit pour les électrons)
 
 # Déclaration de variables physiques "Typical values"
 DIM = 2 #Nombre de degrés de liberté de la simulation 
@@ -33,6 +34,8 @@ T = 300 # around room temperature
 #### CANEVAS DE FOND ####
 L = 1 # container is a cube L on a side
 gray = color.gray(0.7) # color of edges of container and spheres below
+blue = color.blue # color of ionic cores
+red = color.red # color of ionic cores
 animation = canvas( width=750, height=500) # , align='left')
 animation.range = L
 # animation.title = 'Théorie cinétique des gaz parfaits'
@@ -48,7 +51,7 @@ cadre = curve(color=gray, radius=r)
 cadre.append([vector(-d,-d,0), vector(d,-d,0), vector(d,d,0), vector(-d,d,0), vector(-d,-d,0)])
 
 #### POSITION ET QUANTITÉ DE MOUVEMENT INITIALE DES SPHÈRES ####
-Electrons = [] # Objet qui contiendra les sphères pour l'animation
+Spheres = [] # Objet qui contiendra les sphères pour l'animation
 p = [] # quantité de mouvement des sphères
 apos = [] # position des sphères
 pavg = sqrt(2*mass*(DIM/2)*k*T) # average kinetic energy in 3D p**2/(2mass) = (3/2)kT : Principe de l'équipartition de l'énergie en thermodynamique statistique classique
@@ -58,8 +61,8 @@ for i in range(Nelectrons):
     y = L*random()-L/2
     z = 0
     if i == 0:  # garde une sphère plus grosse et colorée parmis toutes les grises
-        Electrons.append(simple_sphere(pos=vector(x,y,z), radius=0.03, color=color.magenta)) #, make_trail=True, retain=100, trail_radius=0.3*Ratom))
-    else: Electrons.append(simple_sphere(pos=vector(x,y,z), radius=Relectron, color=gray))
+        Spheres.append(simple_sphere(pos=vector(x,y,z), radius=0.03, color=color.magenta)) #, make_trail=True, retain=100, trail_radius=0.3*Ratom))
+    else: Spheres.append(simple_sphere(pos=vector(x,y,z), radius=Relectron, color=blue))
     apos.append(vec(x,y,z)) # liste de la position initiale de toutes les sphères
 #    theta = pi*random() # direction de coordonnées sphériques, superflue en 2D
     phi = 2*pi*random() # direction aléatoire pour la quantité de mouvement
@@ -67,6 +70,19 @@ for i in range(Nelectrons):
     py = pavg*sin(phi)
     pz = 0
     p.append(vector(px,py,pz)) # liste de la quantité de mouvement initiale de toutes les sphères
+
+
+### ON INITIALISE LES COEURS IONIQUES STATIQUES ###
+sides = int(np.sqrt(Ncores))
+for i in range(sides):
+    for k in range(sides):
+        x = ((i+0.5)/sides)*L-L/2 # position fixe environ équidistante, mais surtout périodique
+        y = ((k+0.5)/sides)*L-L/2
+        z = 0
+        Spheres.append(simple_sphere(pos=vector(x,y,z), radius=Rcore, color=red))
+        apos.append(vec(x,y,z)) # liste de la position initiale de toutes les sphères
+        p.append(vector(0,0,0)) # liste de la quantité de mouvement initiale de toutes les sphères
+
 
 #initialization = pickle.load(open("PHY-3003/init.pkl", "rb"))
 #apos,p = initialization[0], initialization[1]
@@ -76,11 +92,11 @@ for i in range(Nelectrons):
 #### FONCTION POUR IDENTIFIER LES COLLISIONS, I.E. LORSQUE LA DISTANCE ENTRE LES CENTRES DE 2 SPHÈRES EST À LA LIMITE DE S'INTERPÉNÉTRER ####
 def checkCollisions():
     hitlist = []   # initialisation
-    r2 = 2*Relectron   # distance critique où les 2 sphères entre en contact à la limite de leur rayon
+    r2 = Relectron+Rcore   # distance critique où les 2 sphères entre en contact à la limite de leur rayon
     r2 *= r2   # produit scalaire pour éviter une comparaison vectorielle ci-dessous
     for i in range(Nelectrons):
         ai = apos[i]
-        for j in range(i) :
+        for j in range(Nelectrons, Nelectrons+Ncores) :
             aj = apos[j]
             dr = ai - aj   # la boucle dans une boucle itère pour calculer cette distance vectorielle dr entre chaque paire de sphère
             if mag2(dr) < r2:   # test de collision où mag2(dr) qui retourne la norme élevée au carré de la distance intersphère dr
@@ -154,7 +170,7 @@ for k in tqdm(range(100000)):
         vitesse.append(p[i]/mass)   # par définition de la quantité de nouvement pour chaque sphère
         deltax.append(vitesse[i] * dt)   # différence avant pour calculer l'incrément de position
 
-        Electrons[i].pos = apos[i] = apos[i] + deltax[i]  # nouvelle position de l'atome après l'incrément de temps dt
+        Spheres[i].pos = apos[i] = apos[i] + deltax[i]  # nouvelle position de l'atome après l'incrément de temps dt
 
     #### CONSERVE LA QUANTITÉ DE MOUVEMENT AUX COLLISIONS AVEC LES MURS DE LA BOÎTE ####
     for i in range(Nelectrons):
@@ -168,6 +184,8 @@ for k in tqdm(range(100000)):
 
     #### LET'S FIND THESE COLLISIONS!!! ####
     hitlist = checkCollisions()
+    #print(hitlist)
+    #input()
     #### CONSERVE LA QUANTITÉ DE MOUVEMENT AUX COLLISIONS ENTRE SPHÈRES ####
     for ij in hitlist:
 
@@ -175,8 +193,8 @@ for k in tqdm(range(100000)):
         i = ij[0]  # extraction du numéro des 2 sphères impliquées à cette itération
         j = ij[1]
         ptot = p[i]+p[j]   # quantité de mouvement totale des 2 sphères
-        mtot = 2*mass    # masse totale des 2 sphères
-        Vcom = ptot/mtot   # vitesse du référentiel barycentrique/center-of-momentum (com) frame
+        mtot = 200*mass    # masse totale des 2 sphères
+        Vcom =  p[j]/mass  # vitesse du référentiel barycentrique/center-of-momentum (com) frame
         posi = apos[i]   # position de chacune des 2 sphères
         posj = apos[j]
         vi = p[i]/mass   # vitesse de chacune des 2 sphères
@@ -186,27 +204,22 @@ for k in tqdm(range(100000)):
 
         # exclusion de cas où il n'y a pas de changements à faire
         if vrel.mag2 == 0: continue  # exactly same velocities si et seulement si le vecteur vrel devient nul, la trajectoire des 2 sphères continue alors côte à côte
-        if rrel.mag > Relectron: continue  # one atom went all the way through another, la collision a été "manquée" à l'intérieur du pas deltax
+        if rrel.mag > Rcore: continue  # one atom went all the way through another, la collision a été "manquée" à l'intérieur du pas deltax
 
         # calcule la distance et temps d'interpénétration des sphères dures qui ne doit pas se produire dans ce modèle
         dx = dot(rrel, vrel.hat)       # rrel.mag*cos(theta) où theta is the angle between vrel and rrel:
         dy = cross(rrel, vrel.hat).mag # rrel.mag*sin(theta)
-        alpha = asin(dy/(2*Relectron))  # alpha is the angle of the triangle composed of rrel, path of atom j, and a line from the center of atom i to the center of atom j where atome j hits atom i
-        d = (2*Relectron)*cos(alpha)-dx # distance traveled into the atom from first contact
+        alpha = asin(dy/(Relectron+Rcore))  # alpha is the angle of the triangle composed of rrel, path of atom j, and a line from the center of atom i to the center of atom j where atome j hits atom i
+        d = (Relectron+Rcore)*cos(alpha)-dx # distance traveled into the atom from first contact
         deltat = d/vrel.mag         # time spent moving from first contact to position inside atom
 
         #### CHANGE L'INTERPÉNÉTRATION DES SPHÈRES PAR LA CINÉTIQUE DE COLLISION ####
         posi = posi-vi*deltat   # back up to contact configuration
-        posj = posj-vj*deltat
         pcomi = p[i]-mass*Vcom  # transform momenta to center-of-momentum (com) frame
-        pcomj = p[j]-mass*Vcom
         rrel = hat(rrel)    # vecteur unitaire aligné avec rrel
         pcomi = pcomi-2*dot(pcomi,rrel)*rrel # bounce in center-of-momentum (com) frame
-        pcomj = pcomj-2*dot(pcomj,rrel)*rrel
         p[i] = pcomi+mass*Vcom # transform momenta back to lab frame
-        p[j] = pcomj+mass*Vcom
         apos[i] = posi+(p[i]/mass)*deltat # move forward deltat in time, ramenant au même temps où sont rendues les autres sphères dans l'itération
-        apos[j] = posj+(p[j]/mass)*deltat
 
     # **Appel de la fonction suit() ici**
     iterations_since_last_col, deltapos = follow_particle(deltax, hitlist, deltapos, liste_temps_entre_collision,liste_distance_entre_collision,liste_distance_x_entre_collision, liste_distance_y_entre_collision, liste_distance_z_entre_collision,n_particle=0, iterations_since_last_col=iterations_since_last_col)
