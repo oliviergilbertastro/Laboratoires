1f9ee5730c2f559df7c485b427ae72f018292c84
OlivierGilbert <olgil12@ulaval.ca>
2025-03-12 16:08:44 -0400

    preliminary graphs

--- PHY-3004/Annihilation/graph_preliminaires.py
+++ PHY-3004/Annihilation/graph_preliminaires.py
@@ -17,18 +17,23 @@ def deg_to_rad(degs):
     return np.array(degs)*np.pi/180
 
 angles_theo = np.linspace(-30,30,1000)
-plt.plot(angles_theo, A_normal_theorique(deg_to_rad(angles_theo), DISTANCE_SOURCE/R_DETECTOR, 0), "-", linewidth=3, color="red", label="13cm")
-plt.plot(angles_theo, A_normal_theorique(deg_to_rad(angles_theo), 15/R_DETECTOR, 0), "--", linewidth=2, color="blue", label="15cm")
+ax1 = plt.subplot(111)
+plt.plot(angles_theo, A_normal_theorique(deg_to_rad(angles_theo), DISTANCE_SOURCE/R_DETECTOR, 0), "-", linewidth=3, color="red", alpha=0.5, label="$d=$13cm")
+plt.plot(angles_theo, A_normal_theorique(deg_to_rad(angles_theo), 15/R_DETECTOR, 0), "--", linewidth=2, color="blue", alpha=0.5, label="$d=$15cm")
 plt.hlines(0.5, -30, 30, linestyles="dashed", colors="black")
 
 plt.plot([-22.53, 22.53], [0,0], "o", color="red")
-plt.plot([-9.05,9.05], [0.5,0.5], "o", color="red")
+plt.plot([-9.05,9.05], [0.5,0.5], "*", markersize=10, color="red")
 
 plt.plot([-19.5,19.5], [0,0], "o", color="blue")
-plt.plot([-7.84,7.84], [0.5,0.5], "o", color="blue")
+plt.plot([-7.84,7.84], [0.5,0.5], "*", markersize=10, color="blue")
 
 plt.text(-30, 0.51, r"$N/N_0=50\%$", fontsize=16)
 plt.legend(fontsize=15)
 plt.xlabel(r"$\phi \, \mathrm{[^\circ]}$", fontsize=16)
 plt.ylabel(r"$N/N_0$", fontsize=16)
+ax1.xaxis.set_tick_params(labelsize=15)
+ax1.yaxis.set_tick_params(labelsize=15)
+plt.tight_layout()
+plt.savefig("PHY-3004/Annihilation/Figures/prelim.pdf")
 plt.show()
