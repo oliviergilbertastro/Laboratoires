76c7924d8cf4820c3567dc05580b3eb56c4a460e
Xavier Boucher <xavier.boucher.4@ulaval.ca>
2025-03-31 11:20:02 -0400

    push

--- PHY-3004/Annihilation/x.py
+++ PHY-3004/Annihilation/x.py
@@ -39,12 +39,19 @@ if True:
         # Ajustement des tailles de caractères pour les axes
         plt.xticks(fontsize=18)
         plt.yticks(fontsize=18)
-        plt.show()
+        plt.tight_layout
+        plt.show(block=False)
+
+        # Pause for 5 seconds
+        plt.pause(1)
+
+        # Close the plot
+        plt.close('all')
 
 
 data = load_text(sources[0])
-Na22_data = load_text(sources[1])
-#Na22_data = load_text(sources[2])
+Na22_data_mobile = load_text(sources[1])
+Na22_data_fixe = load_text(sources[2])
 
 
 
@@ -58,7 +65,9 @@ Na22_data = load_text(sources[1])
 # Définir la fonction gaussienne
 def gaussienne(x, A, mu, sigma, B, C):
     return A * np.exp(-((x - mu) ** 2) / (2 * sigma ** 2)) + B * x + C
-
+# Définir la fonction gaussienne
+def linear(x, a, b):
+    return a*x+b
 # Function to perform the Gaussian fit and calculate FWHM
 def fit_gaussian(data, title, source_pic, p0=None, lower_bound=0):
     x = np.arange(len(data))  # Create an x-axis corresponding to the indices of the data
@@ -80,7 +89,8 @@ def fit_gaussian(data, title, source_pic, p0=None, lower_bound=0):
     # Tracer le résultat
     plt.figure(figsize=(10, 6))
     plt.plot(x, data, label="Données", color='blue')
-    plt.plot(x, gaussienne(x, *params), label=f"{title}", color='red', linestyle='--')
+    plt.plot(x, gaussienne(x, *params), label=f"{title}", color='red', linestyle='--',  linewidth=2)
+    plt.plot(x, linear(x, *(B, C)), label=f"Portion linéaire", color='black', linestyle='-', linewidth=2)
 
     plt.xlabel('Canaux', fontsize=18)  
     plt.ylabel('Intensité (Counts)', fontsize=18)
@@ -95,7 +105,14 @@ def fit_gaussian(data, title, source_pic, p0=None, lower_bound=0):
     save_path="PHY-3004/Annihilation/Figures"  
     save_file = os.path.join(save_path, f"{source_pic.replace(' ', '_')}.png")
     plt.savefig(save_file, dpi=300)
-    plt.show()
+    plt.tight_layout
+    plt.show(block=False)
+
+    # Pause for 5 seconds
+    plt.pause(1)
+
+    # Close the plot
+    plt.close('all')
 
     from scipy.integrate import quad
     print(x[0], x[-1],params[0],params[1],params[2], A_err, mu_err, sigma_err)
@@ -136,23 +153,26 @@ Na22_511_bounds = (1110, 1550)
 Na22_1275_bounds = (2900, 3500)
 
 # Isolate the peaks using the defined bounds
-Na22_data_511 = Na22_data[Na22_511_bounds[0]:Na22_511_bounds[1]]
-Na22_data_1275 = Na22_data[Na22_1275_bounds[0]:Na22_1275_bounds[1]]
+Na22_data_511_fixe = Na22_data_fixe[Na22_511_bounds[0]:Na22_511_bounds[1]]
+Na22_data_1275_fixe = Na22_data_fixe[Na22_1275_bounds[0]:Na22_1275_bounds[1]]
 
-
+Na22_data_511_mobile = Na22_data_mobile[Na22_511_bounds[0]:Na22_511_bounds[1]]
+Na22_data_1275_mobile = Na22_data_mobile[Na22_1275_bounds[0]:Na22_1275_bounds[1]]
 
 position_Co57_122, FWHM_Co57_122 = fit_gaussian(Co57_data_122, "Ajustement Gaussien", 'Co57_122' ,p0=[max(Co57_data_122), np.argmax(Co57_data_122), 10, 0, 0], lower_bound=Co57_122_bounds[0])
 position_Cs137_661, FWHM_Cs137_661 = fit_gaussian(Cs137_data_661, "Ajustement Gaussien ",'Cs137_661' ,p0=[max(Cs137_data_661), np.argmax(Cs137_data_661), 10, 0, 0], lower_bound=Cs137_661_bounds[0])
 position_Co60_1173, FWHM_Co60_1173 = fit_gaussian(Co60_data_1173, "Ajustement Gaussien ",'Co60_1173', p0=[max(Co60_data_1173), np.argmax(Co60_data_1173), 10, 0, 0], lower_bound=Co60_1173_bounds[0])
 position_Co60_1333, FWHM_Co60_1333 = fit_gaussian(Co60_data_1333, "Ajustement Gaussien ", 'Co60_1333',p0=[max(Co60_data_1333), np.argmax(Co60_data_1333), 10, 0, 0], lower_bound=Co60_1333_bounds[0])
-position_Na22_511, FWHM_Na22_511 = fit_gaussian(Na22_data_511, "Ajustement Gaussien", 'Na22_511',p0=[max(Na22_data_511), np.argmax(Na22_data_511), 10, 0, 0], lower_bound=Na22_511_bounds[0])
-position_Na22_1275, FWHM_Na22_1275 = fit_gaussian(Na22_data_1275, "Ajustement Gaussien", 'Na22_511',p0=[max(Na22_data_1275), np.argmax(Na22_data_1275), 10, 0, 0], lower_bound=Na22_data_1275[0])
+position_Na22_511_fixe, FWHM_Na22_511_fixe = fit_gaussian(Na22_data_511_fixe, "Ajustement Gaussien", 'Na22_511_fixe',p0=[max(Na22_data_511_fixe), np.argmax(Na22_data_511_fixe), 10, 0, 0], lower_bound=Na22_511_bounds[0])
+position_Na22_1275_fixe, FWHM_Na22_1275_fixe = fit_gaussian(Na22_data_1275_fixe, "Ajustement Gaussien", 'Na22_1275_fixe',p0=[max(Na22_data_1275_fixe), np.argmax(Na22_data_1275_fixe), 10, 0, 0], lower_bound=Na22_1275_bounds[0])
 
 
+position_Na22_511_mobile, FWHM_Na22_511_mobile = fit_gaussian(Na22_data_511_mobile, "Ajustement Gaussien", 'Na22_511_mobile',p0=[max(Na22_data_511_mobile), np.argmax(Na22_data_511_mobile), 10, 0, 0], lower_bound=Na22_511_bounds[0])
+position_Na22_1275_mobile, FWHM_Na22_1275_mobile = fit_gaussian(Na22_data_1275_mobile, "Ajustement Gaussien", 'Na22_1275_mobile',p0=[max(Na22_data_1275_mobile), np.argmax(Na22_data_1275_mobile), 10, 0, 0], lower_bound=Na22_1275_bounds[0])
 
 # Définir une fonction linéaire pour le fit (sans intercept)
-def linear(x, a):
-    return a * x
+def linear(x, a, b):
+    return a * x+b
 
 # Positions des pics (canaux)
 
@@ -168,6 +188,7 @@ params, covariance = curve_fit(linear, positions, energies, sigma=fwhm_x, absolu
 
 # Extraire la pente du fit
 slope = params[0]
+b = params[1]
 
 # Calculer l'erreur standard du fit (écart-type)
 perr = np.sqrt(np.diag(covariance))
@@ -185,10 +206,10 @@ plt.errorbar(positions, energies, xerr=fwhm_x, fmt='o', label="Pics expérimenta
 plt.plot(x, linear(x, *params), label=f"Régression linéaire", color='black', linestyle='--')
 
 # Ajouter le texte avec l'équation de la régression et la valeur de $R^2$
-equation_text = f"$y = {slope:.4f} \cdot x$"
+equation_text = f"$y = {slope:.4f} \cdot x{b:.4f}$"
 r2_text = f"$R^2 = {r_squared:.4f}$"
-plt.text(0.5, 0.8, equation_text, transform=plt.gca().transAxes, fontsize=18, color='black')
-plt.text(0.5, 0.75, r2_text, transform=plt.gca().transAxes, fontsize=18, color='black')
+plt.text(0.05, 0.6, equation_text, transform=plt.gca().transAxes, fontsize=18, color='black')
+plt.text(0.05, 0.55, r2_text, transform=plt.gca().transAxes, fontsize=18, color='black')
 
 # Personnaliser le graphique
 plt.xlabel('Position du pic (canal)', fontsize=18)
@@ -199,9 +220,17 @@ plt.grid(True)
 
 # Ajuster la taille des graduations (ticks)
 plt.tick_params(axis='both', labelsize=18)
+plt.tight_layout
+save_path="PHY-3004/Annihilation/Figures/Fit_lin"  
 
-plt.show()
+plt.savefig(save_path, dpi=300)
+plt.show(block=False)
 
+    # Pause for 5 seconds
+plt.pause(1)
+
+    # Close the plot
+plt.close('all')
 
 # Afficher les résultats dans la console
 print(f"Pente de la régression linéaire (a) : {slope:.4f}")
@@ -260,13 +289,13 @@ f_Na22_pic1275, f_Na22_pic511 = 0.994, 1.78
 
 
 energies*=1000
-energies = np.append(energies, 511)
-positions = np.append(positions, position_Na22_511)
-fwhm_x = np.append(fwhm_x, FWHM_Na22_511)
+energies = np.append(energies, (511, 1275))
+positions = np.append(positions, (position_Na22_511_fixe, position_Na22_1275_fixe))
+fwhm_x = np.append(fwhm_x, (FWHM_Na22_511_fixe, FWHM_Na22_1275_fixe))
 # Affichage des résultats avec valeur absolue de l'écart et de l'écart en %
 for i in range(len(energies)):
-    position_ajustee = positions[i] * pente
-    fwhm_ajuste = fwhm_x[i] * pente
+    position_ajustee = positions[i] * pente+b
+    fwhm_ajuste = fwhm_x[i] * pente+b
     ecart = abs(position_ajustee - energies[i])  # Valeur absolue de l'écart
     ecart_pourcentage = abs((ecart / energies[i]) * 100)  # Valeur absolue de l'écart en %
 
